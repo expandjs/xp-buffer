dc9ddc1558b490b8eaa6792ce61b186ebbce96a5
grullo90 <g.rullo@expandjs.com>
2017-06-14 15:47:02 +0200

    Release 1.0.2

--- webpack.config.js
+++ webpack.config.js
@@ -1,14 +1,14 @@
 // Const
-const webpack = require('webpack');
+const Uglify = require('uglifyjs-webpack-plugin');
 
 // Exporting
 module.exports = {
     entry: './index.js',
     output: {
         filename: 'xp-buffer.js',
-        path: './dist'
+        path: `${__dirname}/dist`
     },
     plugins: [
-        new webpack.optimize.UglifyJsPlugin({compress: {warnings: false}, output: {comments: false}})
+        new Uglify({compress: {warnings: false}, output: {comments: false}})
     ]
 };
